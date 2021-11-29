5916036e75b1bb69573806a9bde314f94bb881cf
cbarrie6 <chrisjbarrie@googlemail.com>
2021-11-29 16:45:09 +0200

    Update hexsticker code

--- man/figures/hexsticker.R
+++ man/figures/hexsticker.R
@@ -4,7 +4,7 @@ s <- sticker("man/figures/quilt.jpg",
              package="quiltr",
              p_size=50,p_color="black",
              s_x=1.05, s_y=.75,
-             s_width=.4, s_height=.5,
+             s_width=.5, s_height=.6,
              h_fill="white", h_color="black",
              dpi = 800,
         filename="man/figures/quiltrhex.png")
